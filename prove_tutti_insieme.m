1b5b6bf70e5eee53bae2fe1ebd58511fffb05e00
Gabu-debug <best.giacognolo@gmail.com>
2025-03-21 16:48:21 +0100

    aggiustamenti formali

--- prove_tutti_insieme.m
+++ prove_tutti_insieme.m
@@ -1,13 +1,15 @@
-%%
-clear all
+%% Scenario 1
+clear 
 close all
 clc
+% Parametri attrattore
 mu=398600;
+
 th0=0;
 thf=2*pi;
 dth=pi/100;
 
-%orbita 1
+% Orbita 1
 a_i=24400.0;
 e_i=0.728300;
 i_i=0.104700;
@@ -15,16 +17,19 @@ OM_i=1.514000;
 om_i=3.107000;
 th_i=1.665000;
 
-plotOrbit(a_i,e_i,i_i,OM_i,om_i,th0,thf,dth,mu)
 
-%orbita finale
+% Numerose manovre
 
+
+% Orbita d'Arrivo
 rr=[-12985.280000 3801.011400 8109.619300]';
 vv=[-0.948600 -6.134000 1.356000]';
 
-[a,e,i,OM,om,th] = car2par(rr,vv,mu);
+[a_f,e_f,i_f,OM_f,om_f,th_f] = car2par(rr,vv,mu);
 
-plotOrbit(a,e,i,OM,om,th0,thf,dth,mu)
+% Plot
+plotOrbit(a_i,e_i,i_i,OM_i,om_i,th0,thf,dth,mu) % Plotto Orbita di Partenza
+plotOrbit(a_f,e_f,i_f,OM_f,om_f,th0,thf,dth,mu) % Plotto Orbita d'Arrivo
 
 
 
