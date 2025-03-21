6598812969776f93077462b33b9c1351d464cf07
eutric <eutric03@gmail.com>
2025-03-21 16:41:16 +0100

    Update prove_tutti_insieme.m

--- prove_tutti_insieme.m
+++ prove_tutti_insieme.m
@@ -3,6 +3,10 @@ clear all
 close all
 clc
 mu=398600;
+th0=0;
+thf=2*pi;
+dth=pi/100;
+
 %orbita 1
 a_i=24400.0;
 e_i=0.728300;
@@ -10,11 +14,11 @@ i_i=0.104700;
 OM_i=1.514000;
 om_i=3.107000;
 th_i=1.665000;
-th0=0;
-thf=2*pi;
-dth=pi/100;
+
 plotOrbit(a_i,e_i,i_i,OM_i,om_i,th0,thf,dth,mu)
+
 %orbita finale
+
 rr=[-12985.280000 3801.011400 8109.619300]';
 vv=[-0.948600 -6.134000 1.356000]';
 
