393b2695252fcad8e063cb5e81ea0919e5044918
muffin-ant <ulajantela@gmail.com>
2025-03-24 11:14:42 +0100

    bitangente e biellittica

---
+++ biellipticTransfer.m
@@ -0,0 +1,34 @@
+function [delta_v1, delta_v2, delta_v3, delta_t1, delta_t2] = biellipticTransfer(orbit_i,orbit_f, ra_t)
+a_i=orbit_i.a;
+e_i=orbit_i.e;
+mu=orbit_i.mu;
+a_f=orbit_f.a;
+e_f=orbit_f.e;
+
+rp_i = a_i*(a_i-e_i);
+rp_t1= rp_i;
+rp_f = a_f*(a_f-e_f);
+rp_t2 = rp_f;
+a_t1 = (ra_t + rp_t1)/2;
+a_t2 = (ra_t + rp_t2)/2;
+
+% calcolo delle velocità 
+vp_i = sqrt(mu)*sqrt(2/rp_i - 1/a_i);
+vp_f = sqrt(mu)*sqrt(2/rp_f - 1/a_f);
+vp_t1 = sqrt(mu)*sqrt(2/rp_t1 - 1/a_t1);
+vp_t2 = sqrt(mu)*sqrt(2/rp_t2 - 1/a_t2);
+va_t1 = sqrt(mu)*sqrt(2/ra_t - 1/a_t1);
+va_t2 = sqrt(mu)*sqrt(2/ra_t - 1/a_t2);
+
+% calcolo il costo 
+delta_v1 = abs(vp_t1 - vp_i);
+delta_v2 = abs(va_t2 - va_t1);
+delta_v3 = abs(vp_f - vp_t2);
+
+% tempo di manovra 
+delta_t1 = pi*sqrt(a_t1^3 / mu);
+delta_t2 = pi*sqrt(a_t2^3 / mu);
+
+
+end
+
---
+++ bitangentTransfer.m
@@ -0,0 +1,90 @@
+function [delta_v1, delta_v2, delta_t] = bitangentTransfer(orbit_i, orbit_f, type)
+arguments
+   orbit_i struct
+   orbit_f struct
+   type (1,:) char {mustBeMember(type, {'pa', 'ap', 'pp', 'aa'})}
+end
+
+a_i=orbit_i.a;
+e_i=orbit_i.e;
+mu=orbit_i.mu;
+a_f=orbit_f.a;
+e_f=orbit_f.e;
+
+switch type 
+    case 'pa'
+        % pericentro -> apocentro
+        rp_i = a_i*(1-e_i);
+        ra_f = a_f*(1+e_f);
+        rp_t = rp_i;
+        ra_t = ra_f;
+        a_t = (ra_t + rp_t)/2;
+
+        vp_i = sqrt(mu)*sqrt(2/rp_i - 1/a_i);
+        vp_t = sqrt(mu)*sqrt(2/rp_t - 1/a_t);
+        va_f = sqrt(mu)*sqrt(2/ra_f - 1/a_f);
+        va_t = sqrt(mu)*sqrt(2/ra_t - 1/a_t);
+
+        % costo manovra 
+        delta_v1 = abs(vp_t - vp_i);
+        delta_v2 = abs(va_f - va_t);
+        delta_t = pi*sqrt(a_t^3 / mu); % tempo di manovra 
+
+    case 'ap'
+        % apocentro -> pericentro 
+        ra_i = a_i*(1+e_i);
+        rp_f = a_f*(1-e_f);
+        ra_t = ra_i;
+        rp_t = rp_f;
+        a_t = (ra_t + rp_t)/2;
+
+        va_t = sqrt(mu)*sqrt(2/ra_t - 1/a_t);
+        vp_t = sqrt(mu)*sqrt(2/rp_t - 1/a_t);
+        va_i = sqrt(mu)*sqrt(2/ra_i - 1/a_i);
+        vp_f = sqrt(mu)*sqrt(2/rp_f - 1/a_f);
+
+        % costo manovra 
+        delta_v1 = va_t - va_i;
+        delta_v2 = vp_f - vp_t;
+        delta_t = pi*sqrt(a_t^3 / mu); % tempo di manovra 
+
+    case 'pp'
+        % pericentro -> pericentro
+        rp_i = a_i*(1-e_i);
+        rp_f = a_f*(1-e_f);
+        rp_t = rp_i;
+        ra_t = rp_f;
+        a_t = (ra_t + rp_t)/2;
+
+        vp_i = sqrt(mu)*sqrt(2/rp_i - 1/a_i);
+        vp_t = sqrt(mu)*sqrt(2/rp_t - 1/a_t);
+        vp_f = sqrt(mu)*sqrt(2/rp_f - 1/a_f);
+        va_t = sqrt(mu)*sqrt(2/ra_t - 1/a_t);
+
+        % costo manovra 
+        delta_v1 = vp_t - vp_i;
+        delta_v2 = vp_f - va_t;
+        delta_t = pi*sqrt(a_t^3 / mu); % tempo di manovra 
+
+    case 'aa'
+        % apocentro -> apocentro
+        ra_i = a_i*(1+e_i);
+        ra_f = a_f*(1+e_f);
+        rp_t = ra_i;
+        ra_t = ra_f;
+        a_t = (ra_t + rp_t)/2;
+
+        vp_t = sqrt(mu)*sqrt(2/rp_t - 1/a_t);
+        va_t = sqrt(mu)*sqrt(2/ra_t - 1/a_t);
+        va_i = sqrt(mu)*sqrt(2/ra_i - 1/a_i);
+        va_f = sqrt(mu)*sqrt(2/ra_f - 1/a_f);
+
+        % costo manovra 
+        delta_v1 = vp_t - va_i;
+        delta_v2 = va_f - va_t;
+        delta_t = pi*sqrt(a_t^3 / mu); % tempo di monovra
+
+end
+
+end
+
