59a27544fe87b63c0d8b1e52e396fd6822a5271c
Gabu-debug <best.giacognolo@gmail.com>
2025-04-04 17:25:50 +0200

    bruh

--- TOF.m
+++ TOF.m
@@ -8,30 +8,32 @@ e = orbit.e;
 mu = orbit.mu;
 T=2*pi*sqrt(a^3/mu);
 
-if th_1 <= pi && th_2>pi
-    
-
-
 
 
 
 % Calcolo le anomalie eccentriche 
 E_1 = 2*atan(sqrt((1-e)/(1+e)) * tan(th_1/2));
 E_2 = 2*atan(sqrt((1-e)/(1+e)) * tan(th_2/2));
-
+if th_1 > pi
+    E_1 = 2*pi+E_1;
+end
+if th_2 > pi
+    E_2 = 2*pi+E_2;
+end
 M_1=E_1-e*sin(E_1);
 M_2=E_2-e*sin(E_2);
 
-n= sqrt(a^3 /mu);
+n= sqrt(mu/(a^3));
 
 
-t_1=M_1/n;
-t_2=M_2/n;
-
+t_1=M_1/n
+t_2=M_2/n
+if th_2 < th_1
+    delta_t = t_2-t_1+T;
+else
+    delta_t = t_2-t_1;
+end
 % Il tempo di volo
 
-if th_1 > th_2
-    
-    delta_t=t_2-t_1+T;
 
-   
+end
