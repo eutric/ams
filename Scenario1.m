d52782df503fd648e76c3deb27285e16285e6e1a
antela-ulaj <ulajantela@gmail.com>
2025-04-06 17:32:09 +0200

    ulteriori commenti

--- Scenario1.m
+++ Scenario1.m
@@ -49,7 +49,46 @@ plotOrbit(orbit_cp,th0,thf,dth,['--', 'r'])
 [delta_v2, th_i, th_f, th_best, orbit_chper] = change_pericentre_arg(orbit_cp, O_end.om, th_cp);
 plotOrbit(orbit_chper,th0,thf,dth,['--', 'g'])
 
+% Effettuo il trasferimento di orbita 
+[delta_v1_bt, delta_v2_bt, delta_t_bt, orbit_bt] = bitangentTransfer(orbit_chper, O_end, 'pa');
+plotOrbit(orbit_bt,th0,pi,dth,['-', 'c'])
+% non penso abbiano molto senso i trasferimenti aa o pp per come sono messe
+% le orbite 
 
-[delta_v1_bt, delta_v2_bt, delta_t_bt] = bitangentTransfer(orbit_chper, O_end, 'pa');
 
+
+% HO PROVATO A FAR GIRARE UN SATELLITE IN MANIERA APPROSSIMATIVA PERCHE' VOLEVO VEDERLO MUOVERSI 
+% MA NON SI MUOVE COME DOVREBBE, VA AL CONTRARIO E NON SEGUE LEGGI FISICHE 
+
+a = 24400.0;
+e = 0.728300;
+theta = linspace(0, 2*pi, 200); 
+r = (a * (1 - e^2)) ./ (1 + e * cos(theta));
+x = r .* cos(theta);
+y = r .* sin(theta);
+r2 = [x; y; zeros(length(x),1)'];
+
+
+R_OM = [cos(O_start.OM) sin(O_start.OM) 0;
+    -sin(O_start.OM) cos(O_start.OM) 0;
+    0 0 1];
+R_i = [1 0 0;
+    0 cos(O_start.i) sin(O_start.i);
+    0 -sin(O_start.i) cos(O_start.i)];
+R_om = [cos(O_start.om) sin(O_start.om) 0;
+    -sin(O_start.om) cos(O_start.om) 0;
+    0 0 1];
+
+T = R_om*R_i*R_OM;
+rr2 = T' * r2; 
+
+
+ss = plot3(rr2(1,:), rr2(2,:), rr2(3,:), 'ro', 'MarkerFaceColor', 'r', 'MarkerSize', 8);
+
+for k = 1:length(theta)
+    set(ss, 'XData', rr2(1,k), 'YData', rr2(2,k), 'ZData', rr2(3,k));
+
+    pause(0.01); 
+    drawnow; 
+end
 
--- TOF.m
+++ TOF.m
@@ -1,4 +1,6 @@
 function delta_t = TOF(orbit, th_1, th_2)
+% La funzione prende in ingresso semiasse maggiore, eccentricità di
+% un'orbita e blah blah blah
 % Time of Flight
 % Algoritmo valido per orbite 0<e<1
 % Problema indiretto 
@@ -26,14 +28,13 @@ M_2=E_2-e*sin(E_2);
 n= sqrt(mu/(a^3));
 
 
-t_1=M_1/n
-t_2=M_2/n
+t_1=M_1/n;
+t_2=M_2/n;
 if th_2 < th_1
     delta_t = t_2-t_1+T;
 else
     delta_t = t_2-t_1;
 end
-% Il tempo di volo
 
 
 end
